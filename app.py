b0ce79944b8449a9b923c45ccfeaca1fa09b01bf
Simon Fraser <sfraser@mozilla.com>
2017-04-04 15:47:01 +0100

    updates for readability

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import pprint
 from flask import Flask, g
 from flask_oidc import OpenIDConnect
 
@@ -17,24 +18,22 @@ USER_FIELDS = ["openid", "profile", "offline_access", "name", "given_name", "fam
 def index():
     if not oidc.user_loggedin:
         return "Not logged in"
-
-    data = "Welcome {}\n<br />".format(oidc.user_getfield('email'))
-
-    data += "oidc_id_token: {}\n<br />".format(g.oidc_id_token)
-
-    data += "user_getinfo: {}\n<br />".format(oidc.user_getinfo(USER_FIELDS))
-    data += "g._oidc_userinfo: {}\n<br />".format(g._oidc_userinfo)
-    return data
+    return "Welcome"
     
 
 
 @app.route('/login')
 @oidc.require_login
 def login():
-    return "Welcome {}".format(oidc.user_getfield('email'))
+    data = "Welcome {}\n<br /><br />".format(oidc.user_getfield('email'))
+    data += "oidc_id_token: {}\n<br /><br />".format(pprint.pformat(g.oidc_id_token).replace('\n', '<br />'))
+    data += "user_getinfo: {}\n<br /><br />".format(pprint.pformat(oidc.user_getinfo(USER_FIELDS)).replace('\n', '<br />'))
+    data += "user_getinfo for groups: {}\n<br /><br />".format(pprint.pformat(oidc.user_getinfo(['groups'])).replace('\n', '<br />'))
+    #data += "g._oidc_userinfo: {}\n<br /><br />".format(pprint.pformat(g._oidc_userinfo).replace('\n','<br />'))
+    return data
 
 
 @app.route('/api')
 @oidc.accept_token()
 def test_api():
-    return json.dumps('Welcome %s' % g.oidc_token_info['sub'])
+    return json.dumps(oidc.user_getinfo(['email', 'groups']))
